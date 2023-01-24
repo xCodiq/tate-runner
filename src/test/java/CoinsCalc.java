365f5b921be88014cc4d2f15d4ed87a97d900ee1
xCodiq <elmarblume91@gmail.com>
2023-01-24 15:22:21 +0100

    Nino coin calc

---
+++ src/test/java/CoinsCalc.java
@@ -0,0 +1,17 @@
+public class CoinsCalc {
+
+	public CoinsCalc() {
+		int score = 205;
+
+		int coins = 0;
+
+		//
+
+		System.out.println("coins = " + coins);
+
+	}
+
+	public static void main(String[] args) {
+		new CoinsCalc();
+	}
+}
